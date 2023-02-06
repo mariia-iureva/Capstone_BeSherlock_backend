eca88668d64412d744039aa24e9625bffe338717
Masha <iiureva.mariia@gmail.com>
2023-02-05 23:23:43 -0800

    incapsulated reply function, both web App and Telegram work

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const { enableLogging } = require("./logging");
-// import { enableLogging } from "./logging.js";
+// these are for logging and working wth files
+const fs = require("fs");
+const util = require("util");
 
-// var fs = require("fs");
-// var util = require("util");
-
+// these are for express server
 const express = require("express");
 const cors = require("cors");
+
+// const bodyParser = require("body-parser");
+// this one is for env secret config
 // require("dotenv").config();
 
+// this will also go to secret env
 const PORT = process.env.PORT || 3001;
+// const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_BOT_TOKEN = "5969404483:AAEQ6uAhw7uQh0OwLFGf6dSvC9d0acP2rFc";
+
+const TelegramBot = require("node-telegram-bot-api");
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: false });
+
 const app = express();
 const { NlpManager } = require("node-nlp");
 
@@ -17,10 +26,34 @@ manager = new NlpManager();
 app.use(cors());
 app.use(express.json()); // JSON encoded body
 
+// _______LOGGING________
+var log_file = fs.createWriteStream(__dirname + "/debug.log", { flags: "a" });
+var log_stdout = process.stdout;
+
+console.log = function (d) {
+  log_file.write(util.format(d) + "\n");
+  log_stdout.write(util.format(d) + "\n");
+};
+
+let date = new Date();
+let timestamp =
+  date.getDate() +
+  "/" +
+  (date.getMonth() + 1) +
+  "/" +
+  date.getFullYear() +
+  "@" +
+  date.getHours() +
+  ":" +
+  date.getMinutes() +
+  ":" +
+  date.getSeconds();
+
+console.log(`Bot logging on ${timestamp}`);
+// ___END OF LOGGING______
+
 // Loading nlp model for answers:
 manager.load("./model.nlp");
-
-enableLogging;
 
 app.get("/", function (req, res) {
   res.send("Hi! I am Masha's game ChatBot.");
@@ -28,46 +61,30 @@ app.get("/", function (req, res) {
 
 app.post("/postMessage", async function (req, res) {
   let message = req.body.message.toLowerCase();
-  console.log("[INFO](" + timestamp + ") Msg from User: " + message);
-  // console.log("message from User:" + message);
+  const reply = await getReply(message);
+  console.log("[INFO](" + timestamp + ") Msg from Bot: " + reply);
+  res.send({ bot_message: reply });
+});
 
+app.post("/postMessageFromTelegram", async (req, res) => {
+  const reply = await getReply(req.body.message.text);
+  console.log("[INFO](" + timestamp + ") Msg from Bot: " + reply);
+
+  // return the same message back
+  bot.sendMessage(req.body.message.chat.id, reply);
+  return res.status(200).json();
+});
+
+const getReply = async (message) => {
   const reply = await manager.process("en", message);
 
   if (reply.intent === "None") {
     const reply = await manager.process("en", "Tell me you don't know");
-    res.send({ bot_message: reply.answer });
-    console.log("[INFO](" + timestamp + ") Msg from Bot: " + reply.answer);
-    return;
+    return reply.answer;
   }
 
-  // console.log(reply);
-  console.log("[INFO](" + timestamp + ") Msg from Bot: " + reply.answer);
-  res.send({ bot_message: reply.answer });
-});
-
-// function getReply(message) {
-//   let found = null;
-
-//   fastAnswers.forEach((fastAnswer) => {
-//     //For every fast answer
-//     fastAnswer.triggers.forEach((trigger) => {
-//       //Check among all triggers
-//       let regex = new RegExp("\\b" + trigger + "\\b", "gi"); //Search global and case insenstive
-//       let regexResult = message.match(regex);
-
-//       //console.log("Regex result: " + regexResult);
-
-//       if (regexResult != null && !found) {
-//         //If RegEx matches and wasn't previously found
-//         let rnd = Math.floor(Math.random() * fastAnswer.replies.length + 0);
-//         found = fastAnswer.replies[rnd];
-//         //return fastAnswer.replies[rnd]; //Can't do this with forEach (ahw man, that sucks), see comment above, substitute forEach with for
-//       }
-//     });
-//   });
-
-//   return found;
-// }
+  return reply.answer;
+};
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
